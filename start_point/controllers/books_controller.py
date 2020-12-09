f0d1942b316ce577ae73e484efc319e8abfd739f
Bela <belahamid@gmail.com>
2020-12-09 15:32:32 +0000

    initial commit

---
+++ start_point/controllers/books_controller.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+from repositories import author_repository
+from repositories import book_repository
+from models.author import Author
+from models.book import Book
+
+
+
+
+
+
+# NEW
+# GET '/books/new'
+
+# CREATE
+# POST '/books'
+
+## SHOW
+# GET '/books/<id>'
+
+# EDIT
+# GET '/books/<id>/edit'
+
+# UPDATE
+# PUT '/books/<id>'
+
+## DELETE
+# DELETE '/books/<id>'
+
